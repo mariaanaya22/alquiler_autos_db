345baef403716b3b9fbc58f1175e4502aa9d3ac9
kathe <vasquezdianakatherine@gmail.com>
2024-12-05 13:59:47 -0500

    backend de la app alquiler de autos

---
+++ controller/clientecontroller.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+const { Cliente } = require('../models');
+
+ //login
+
+  ////////////////////////////
+exports.registrarCliente = async (req, res) => {
+  try {
+    const { nombre, correo, numLic, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 es el número de salt rounds
+    const nuevoCliente = await Cliente.create({ nombre, correo, numLic, password: hashedPassword });
+    res.status(201).json(nuevoCliente);
+  } catch (error) {
+    console.error("Error al crear el cliente:", error);
+    res.status(500).json({ mensaje: "Error al crear el cliente", error: error.message });
+  }
+};
+
+exports.verclientes = async (req, res) => {
+  try {
+    const clientes = await Cliente.findAll();
+    res.json(clientes);
+  } catch (error) {
+    console.error("Error al obtener los clientes:", error);
+    res.status(500).json({ mensaje: "Error al obtener los clientes", error: error.message });
+  }
+};
