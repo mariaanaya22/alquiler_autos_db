6231c117fa7c7efb61a02e7ac4d3ee7108e46806
marianaya22 <mafeanaya22@gmail.com>
2025-03-04 22:17:50 -0500

    prueba

--- routes/clienteroutes.js
+++ routes/clienteroutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const clienteController = require('../controller/clientecontroller');
+router.get('/', clienteController.verclientes);
 router.post('/login', clienteController.loginCliente);
 router.post('/', clienteController.registrarCliente);
-router.get('/', clienteController.verclientes);
+
 
 module.exports = router;
